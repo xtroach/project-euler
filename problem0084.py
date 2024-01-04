9c6fd6843e3af0878bede7a4477d7f049727704d
Friedric Kriegsheim <friedrich.kriegsheim@ika.rwth-aachen.de>
2024-01-04 09:39:45 +0100

    started working on community chests

--- problem0084.py
+++ problem0084.py
@@ -16,11 +16,58 @@ community_chest_indexes = [get_field_index("CC1"),get_field_index("CC2"),get_fie
 visits = [0 for _ in range(0,40)]
 
 for i in range(iterations):
-    
+    visits[pos] += 1 
     dice_one = random.randrange(1,num_sides+1)
     dice_two = random.randrange(1,num_sides+1) 
 
     if dice_one == dice_two:
         pasch_counter += 1
     
-print(visits)
+    if pasch_counter == 3:
+        pasch_counter = 0
+        pos = 0
+        continue
+    
+    dice_sum = dice_one + dice_two
+
+    pos = (pos+dice_sum)%len(field_names)
+
+    if pos in chances_indexes:
+        pass
+    elif pos in community_chest_indexes:
+        roll = random.randrange(1,17)
+        if roll == 1:
+            # Advance to GO
+            pos = get_field_index("GO")
+            continue
+        elif roll == 2: 
+            # Go to JAIL
+            pos = get_field_index("JAIL")
+            continue
+        elif roll == 3:
+            #Go to C1
+            pos = get_field_index("C1")
+            continue
+        elif roll == 4:
+            #Go to E3
+            pos = get_field_index("E3")
+        elif roll == 5:    Advance to GO
+    Go to JAIL
+    Go to C1
+    Go to E3
+    Go to H2
+    Go to R1
+    Go to next R (railway company)
+    Go to next R
+    Go to next U (utility company)
+    Go back 3 squares.
+
+            #Go to H2
+            pos = get_field_index("H2")
+        elif roll == 6:
+            #Go to R1
+            pos = get_field_index("R1")
+    Go to next R (railway company)
+    Go to next R
+    Go to next U (utility company)
+    Go back 3 squares.
